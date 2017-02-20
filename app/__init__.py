d3ead876a9fd298689dbf4803dfabb423145a53f
adrene <adrenemcintosh@yahoo.com>
2017-02-20 04:22:59 +0000

    inital commit

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+# Config Values
+USERNAME = 'admin'
+PASSWORD = 'password123'
+
+# SECRET_KEY is needed for session security, the flash() method in this case stores the message in a session
+SECRET_KEY = 'Sup3r$3cretkey'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+from app import views
+
+UPLOAD_FOLDER = '.app/static/uploads'
+app.config ['UPLOAD FOLDER']=UPLOAD_FOLDER
